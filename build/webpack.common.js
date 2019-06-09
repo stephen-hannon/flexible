46cc8ae0859628f5298ad663935ae98fc77de89f
Stephen Hannon <stephenhannon4@gmail.com>
2019-06-08 23:06:41 -0500

    Adds autoprefixer

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -19,6 +19,8 @@ module.exports = (devMode) => ({
 			}, {
 				loader: 'css-loader'
 			}, {
+				loader: 'postcss-loader'
+			}, {
 				loader: 'sass-loader',
 				options: {
 					implementation: require('sass'),
